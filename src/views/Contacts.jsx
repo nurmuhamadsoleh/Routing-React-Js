76f224a8769ba45e3436f3d0496f04c91c5656a4
Nur  Muhamad Soleh <nursoleh33@gmail.com>
2022-10-10 09:52:56 +0700

    update contact.jsx

--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../parts/Navbar";
 import { Link } from "react-router-dom";
 
 const Contacts = () => {
-  let [data, setSaa] = useState();
   return (
     <>
       <Navbar />
